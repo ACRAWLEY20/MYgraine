077858665d3048b64016ad46daf95f81ffd59786
Anthony Crawley <anthonycrawley@Anthonys-MacBook.local>
2020-07-07 13:52:08 -0500

    Getting Started

---
+++ MYgraine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MYgraine
+//
+//  Created by Anthony Crawley on 7/7/20.
+//  Copyright © 2020 Anthony Crawley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
