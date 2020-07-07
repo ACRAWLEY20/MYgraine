0d70f60d270925bcb983c99663a5033484660640
Anthony Crawley <anthonycrawley@Anthonys-MacBook.local>
2020-07-07 14:03:10 -0500

    Finishing Project Set Up

---
+++ MYgraine/Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  MYgraine
+//
+//  Created by Anthony Crawley on 7/7/20.
+//  Copyright © 2020 Anthony Crawley. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
